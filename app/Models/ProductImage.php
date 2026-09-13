d7dbc12133c878b6ddf2d59969b003d64a2e23df
Loliver1l <loliver1@my.bridgevalley.edu>
2026-09-13 08:19:13 +0300

    Create ProductImage model with relationships and attributes

---
+++ app/Models/ProductImage.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ProductImage extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'product_id',
+        'image_path',
+        'alt_text',
+        'sort_order',
+        'is_primary',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'is_primary' => 'boolean',
+        ];
+    }
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+}
