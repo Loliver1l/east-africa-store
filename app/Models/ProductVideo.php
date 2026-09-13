09206c6ca413e7b1a9800295c5064134dcf91d4a
Loliver1l <loliver1@my.bridgevalley.edu>
2026-09-13 08:20:03 +0300

    Create ProductVideo model with relationships and fillable fields

---
+++ app/Models/ProductVideo.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ProductVideo extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'product_id',
+        'video_path',
+        'thumbnail_path',
+        'mime_type',
+        'sort_order',
+    ];
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+}
